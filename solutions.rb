21669d417db121ab0796162fcc1c47ca9fbb325c
Will Piers <wpiers0405@gmail.com>
2013-10-11 21:51:05 -0600

    problem 10 with new Sieve of Eratosthenes approach

--- solutions.rb
+++ solutions.rb
@@ -2,23 +2,6 @@ require 'set'
 require_relative './euler_methods'
 timer_start = Time.now
 
-# ########################## Problem Ten
-# primes = [2]
-# largest_prime = 2
-# candidate = 3
-# sum = 0
-# while largest_prime < 2000000
-# # The least_factor mehtod was defined for use in problem Three
-# 	if candidate == candidate.least_factor
-# 		primes.push candidate
-# 		sum += candidate
-# 	end
-# 	candidate += 1
-# 	largest_prime = primes.last
-# end
-# puts sum - primes.last
-# puts primes_under(2000000).reduce(:+)
-
 
 # ########################## Problem Twelve
 # num = 1
---
+++ solutions/10.rb
@@ -0,0 +1,20 @@
+require_relative '../core_ext/integer'
+require_relative '../euler_methods'
+include EulerMethods
+
+# primes = [2]
+# largest_prime = 2
+# candidate = 3
+# sum = 0
+# while largest_prime < 2000000
+# # The least_factor mehtod was defined for use in problem Three
+# 	if candidate == candidate.least_factor
+# 		primes.push candidate
+# 		sum += candidate
+# 	end
+# 	candidate += 1
+# 	largest_prime = primes.last
+# end
+# puts sum - primes.last
+
+puts primes_under(2000000).reduce(:+)
